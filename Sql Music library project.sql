171450d99c2ceda38dee417d538c190cf65ed76a
S Karthik <133492829+analytics186@users.noreply.github.com>
2023-05-25 23:14:04 +0530

    Sql Music library project.sql

--- Sql Music library project.sql
+++ Sql Music library project.sql
@@ -114,24 +114,6 @@ amount of purchases. Write a query that returns each country along with
 the top Genre. For countries where the maximum number of purchases
 is shared return all Genres.
 
-method 1:
-
-WITH popular_genre AS
-(
-SELECT COUNT (invoice_line.quantity) AS purchases, customer.country, genre.name, genre.genre_id,
-ROW_NUMBER () OVER (PARTITION BY customer.country ORDER BY COUNT (invoice_line.quantity) DESC) AS RowNo
-FROM invoice_line
-JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-JOIN customer ON customer.customer_id = invoice.customer_id
-JOIN track ON track.track_id = invoice_line.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-GROUP BY 2,3,4
-ORDER BY 2 ASC, 1 DESC
-)
-SELECT * FROM popular_genre WHERE RowNo <= 1
-
-
-method 2:
 
 WITH RECURSIVE
 sales_per_country AS (
@@ -154,36 +136,12 @@ JOIN max_genre_per_country ON sales_per_country.country = max_genre_per_country.
 WHERE sales_per_country. purchases_per_genre = max_genre_per_country.max_genre_number
 
 
-81
-/
+
 Q3: Write a query that determines the customer that has spent the most
 on music for each country. Write a query that returns the country along
 with the top customer and how much they spent. For countries where
 the top amount spent is shared, provide all customers who spent this
 amount
-
-Method 1:
-
-WITH RECURSIVE
-customter_with_country AS (
-SELECT customer.customer_id, first_name,last_name, billing_country, SUM (total) AS total_spending
-FROM invoice
-JOIN customer ON customer.customer_id = invoice.customer_id
-GROUP BY 1,2,3,4
-ORDER BY 2,3 DESC),
-country_max_spending AS (
-SELECT billing_country, MAX(total_spending) AS max_spending
-FROM customter_with_country
-GROUP BY billing_country)
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
-WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
-
-
-Method 2:
 
 WITH Customter_with_country AS (
 SELECT customer.customer_id, first_name,last_name, billing_country, SUM (total) AS total_spending,
